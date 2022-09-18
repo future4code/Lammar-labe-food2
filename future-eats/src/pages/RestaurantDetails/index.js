1238fa2d91b42d22b513d22ca88a8c610aa18bc1
Arissa Yasmin Iwashiro Otsu <arissaotsu@gmail.com>
2022-09-17 23:03:55 -0300

    Início da  página de detalhes

--- future-eats/src/pages/RestaurantDetails/index.js
+++ future-eats/src/pages/RestaurantDetails/index.js
@@ -1,10 +1,71 @@
-import React from 'react';
+import React, { useState, useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import {baseUrl, tempToken} from "../../constants/constants"
+import axios from "axios";
+import { Navbar, Title } from '../../components/navbar/styled';
+import {Adress, DeliveryTime, Image, RestaurantCategory, RestaurantName, Shipping} from '../RestaurantDetails/styled' 
+
 
 function RestaurantDetails () {
+    const pathParams = useParams()
+    const [restaurantDetail, setRestaurantDetail] = useState([])
+
+    useEffect(() => {
+        getRestaurantDetails()
+    }, [])
+
+    const token = localStorage.getItem('token')
+
+    const getRestaurantDetails = () => {
+        axios.get(`${baseUrl}restaurants/${pathParams.id}`, {
+                headers: {
+                    auth: tempToken,
+                }
+            })
+            .then((response) => {
+                console.log(response)
+                setRestaurantDetail(response.data.restaurant)
+            })
+            .catch((error) => {
+                console.log(error)
+            })
+    }
+    
 
     return (
         <div>
+            <Navbar>
+                <Title>Restaurante</Title>
+            </Navbar>
+            {restaurantDetail && 
+            <div>
+                <Image src={restaurantDetail.logoUrl}></Image>
+                <RestaurantName>{restaurantDetail.name}</RestaurantName> 
+                <br></br>
+                <RestaurantCategory> {restaurantDetail.category} </RestaurantCategory> 
+                <br></br>
+                <DeliveryTime> {restaurantDetail.deliveryTime} min </DeliveryTime>
+                <Shipping> Frete R${restaurantDetail.shipping}</Shipping>
+                <br></br>
+                <Adress>{restaurantDetail.address}</Adress>
+            </div>
+            }
 
+            <div>
+                {restaurantDetail.products && restaurantDetail.products.map((product)=> {
+                    return (
+                        <div key={product.id}> 
+                        <img src={product.photoUrl}></img>
+                        {product.name} 
+                        {product.description}
+                        {product.price}
+                        </div>
+                    )
+                })}
+            </div>
+
+           
+            
         </div>
     )
 }
---
+++ future-eats/src/pages/RestaurantDetails/styled.js
@@ -0,0 +1,77 @@
+import styled from "styled-components";
+
+export const Image = styled.img `
+    width: 328px;
+    height: 120px;
+    margin: 17px 16px 12px;
+    object-fit: contain;
+`
+
+export const RestaurantName =styled.span `
+    width: 328px;
+    height: 18px;
+    margin: 12px 16px 8px;
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    color: var(--mid-green);
+` 
+export const RestaurantCategory =styled.span`
+    width: 104px;
+    height: 18px;
+    margin: 8px 8px 8px 16px;
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    color: var(--greyish);
+`
+
+export const DeliveryTime = styled.span` 
+    width: 104px;
+    height: 18px;
+    margin: 8px 8px 10px 16px;
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    color: var(--greyish);
+`
+export const Shipping = styled.span`
+    width: 104px;
+    height: 18px;
+    margin: 8px 128px 10px 8px;
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    color: var(--greyish);    
+`
+
+export const Adress = styled.span `
+    width: 328px;
+    height: 18px;
+    margin: 10px 16px 16px;
+    font-family: Roboto;
+    font-size: 16px;
+    font-weight: normal;
+    font-stretch: normal;
+    font-style: normal;
+    line-height: normal;
+    letter-spacing: -0.39px;
+    color: var(--greyish);
+
+`
